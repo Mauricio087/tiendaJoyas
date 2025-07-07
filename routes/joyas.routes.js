2c4cbb5b859dcc6de03c19568d4ecded45195483
mauricio087 <m.river.cea@gmail.com>
2025-07-07 09:48:30 -0400

    primer commit

---
+++ routes/joyas.routes.js
@@ -0,0 +1,10 @@
+
+import { Router } from 'express';
+import { getJoyasController, getJoyasByFilterController } from '../controllers/joyas.controller.js';
+
+const router = Router();
+
+router.get('/joyas', getJoyasController);
+router.get('/joyas/filtros', getJoyasByFilterController);
+
+export default router;
